c706e94f1325743f945eaa147fc538a2ac72ac52
Luis Alba <LuisAlba@Luiss-MacBook-Pro-2.local>
2017-07-19 00:54:08 -0700

    Added Cucumber tests for logging out

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,8 +13,8 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the home\s?page$/
-      '/'
+    when /^the welcome\s?page$/
+      welcome_path
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
